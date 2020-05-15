327d5ab207d5067a9441dd6b1881ae0beda976d0
alicio <alicio.fcn@gmail.com>
2020-05-15 16:06:39 -0300

    tipos de variaveis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'] // aqui pode ser ser incluido varios css que esse componente tem
 })
 export class AppComponent {
-  title = 'todo';
+  public todos : any[] = []; // [] vazio
+  //public todos : any[];  undefined
+
+  /**
+   * chamado quando todo vez que o componente inicia
+   */
+  constructor() {
+    //com o this. tem acesso a tudo que esta dentro da classe.
+    this.todos.push('passear com cachorro');
+    this.todos.push('ir ao super mercado');
+    this.todos.push('cortar cabelo');
+    this.todos.push(1999);
+    this.todos.push([{message : 'teste'}]);
+    this.todos.push(new Date());
+
+    //em objetos do tipo any é possivel colocar qq coisa
+    // é o bom que fica possivel adicionar qq coisa, o ruim que fica sem tipo
+  }
+
+  /* exemplo para o this
+  login(){
+
+  }
+
+  public teste : number = 0;
+
+  */
+
+
 }
